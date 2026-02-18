c75ada9ef3e1685bf10561ebd1285c53c42b492e
Pranav Mannem <mannp24@students.cs.ubc.ca>
2026-02-17 18:42:36 -0600

    Tests For: Retrieve list of courses

--- test/App.spec.ts
+++ test/App.spec.ts
@@ -10,6 +10,7 @@ const {
 	// CREATED, // 201
 	// NO_CONTENT, // 204
 	// NOT_FOUND, // 404
+	BAD_REQUEST, // 400
 } = StatusCodes;
 
 // Do not change datadir
@@ -31,4 +32,88 @@ describe("REST API v1", function () {
 		expect(res).to.have.property("status", OK);
 		expect(res).to.have.property("text", "App is running!");
 	});
+
+	it("GET /api/v1/courses - Expected: OK - Default", async () => {
+		const res = await request(app).get("/api/v1/courses");
+		expect(res).to.have.property("status", OK);
+		expect(res).to.have.deep.property("body", {
+			total: 0,
+			limit: 100,
+			offset: 0,
+			items: [],
+		});
+	});
+
+	it("GET /api/v1/courses - Expected: OK - In Bounds", async () => {
+		const res = await request(app).get("/api/v1/courses?limit=2000&offset=5");
+		expect(res).to.have.property("status", OK);
+		expect(res).to.have.deep.property("body", {
+			total: 0,
+			limit: 2000,
+			offset: 5,
+			items: [],
+		});
+	});
+
+	it("GET /api/v1/courses - Expected: OK - On Bounds", async () => {
+		const resLow = await request(app).get("/api/v1/courses?limit=1&offset=0");
+		expect(resLow).to.have.property("status", OK);
+		expect(resLow).to.have.deep.property("body", {
+			total: 0,
+			limit: 1,
+			offset: 0,
+			items: [],
+		});
+
+		const resHi = await request(app).get("/api/v1/courses?limit=5000&offset=0");
+		expect(resHi).to.have.property("status", OK);
+		expect(resHi).to.have.deep.property("body", {
+			total: 0,
+			limit: 5000,
+			offset: 0,
+			items: [],
+		});
+	});
+
+	it("GET /api/v1/courses - Expected: OK - Bounds +1", async () => {
+		const resLow = await request(app).get("/api/v1/courses?limit=2&offset=1");
+		expect(resLow).to.have.property("status", OK);
+		expect(resLow).to.have.deep.property("body", {
+			total: 0,
+			limit: 2,
+			offset: 1,
+			items: [],
+		});
+
+		const resHi = await request(app).get("/api/v1/courses?limit=5001&offset=1");
+		expect(resHi).to.have.property("status", BAD_REQUEST);
+		expect(resHi).to.have.deep.property("body", {
+			error: "Invalid request parameters",
+			params: {
+				limit: "expected an integer between 1 and 5000",
+			}
+		});
+	});
+
+	it("GET /api/v1/courses - Expected: OK - Bounds -1", async () => {
+		const resLow = await request(app).get("/api/v1/courses?limit=0&offset=-1");
+		expect(resLow).to.have.property("status", BAD_REQUEST);
+		expect(resLow).to.have.deep.property("body", {
+			error: "Invalid request parameters",
+			params: {
+				limit: "expected an integer between 1 and 5000",
+				offset: "expected an integer >= 0"
+			}
+		});
+
+		const resHi = await request(app).get("/api/v1/courses?limit=4999&offset=-1");
+		expect(resHi).to.have.property("status", BAD_REQUEST);
+		expect(resHi).to.have.deep.property("body", {
+			error: "Invalid request parameters",
+			params: {
+				offset: "expected an integer >= 0",
+			}
+		});
+	});
+
 });
