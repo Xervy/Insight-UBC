02d99887ef605684d3349fd00bd3dd6bdf003021
Pranav Mannem <mannp24@students.cs.ubc.ca>
2026-03-21 01:17:29 -0500

    Implementation for Retrieve a list of buildings

--- src/App.ts
+++ src/App.ts
@@ -1119,6 +1119,7 @@ export async function createApp(config: AppConfig): Promise<Application> {
 	app.get("/api/v2/buildings", async (req, res) => {
 		const file = await fs.readFile(DATA_FILE, "utf-8");
 		const data = JSON.parse(file) as Data;
+		const buildings = data.facilities;
 
 		let limit = parseInt(req.query.limit as string);
 		let offset = parseInt(req.query.offset as string);
@@ -1129,61 +1130,20 @@ export async function createApp(config: AppConfig): Promise<Application> {
 			res.send(400).json(errorRes);
 			return;
 		}
-
-
-		const buildings = data.facilities;
+		
 		const buildingsWithLinks = UpdateListOfBuildingsLinks(buildings);
+
+		const sort = [...buildings].sort((a, b) => a.id.localeCompare(b.id));
+		let items = sort.slice(offset, offset + limit);
+
+		res.status(200).json({
+			total: buildings.length,
+			limit,
+			offset,
+			items: buildingsWithLinks
+		});
 	});
 
 
 	return app;
 }
-
-
-app.get("/api/v1/courses", async (req, res): Promise<void> => {
-	const data = await readData();
-
-	const errorMessage = {
-		error: "Invalid request parameters",
-		params: {} as any,
-	};
-	let isError = false;
-
-	let limit = parseInt(req.query.limit as string);
-	let offset = parseInt(req.query.offset as string);
-	if (isNaN(limit)) {
-		limit = 100;
-	}
-	if (limit < 1 || limit > 5000) {
-		errorMessage.params["limit"] = "expected an integer between 1 and 5000";
-		isError = true;
-		// res.status(400).json({ error: "limit must be between 1 and 5000" });
-		// return;
-	}
-	if (isNaN(offset)) {
-		offset = 0;
-	}
-
-	if (offset < 0) {
-		errorMessage.params["offset"] = "expected an integer >= 0";
-		isError = true;
-		// res.status(400).json({ error: "offset must be >= 0" });
-		// return;
-	}
-
-	if (isError) {
-		res.status(400).json(errorMessage);
-		return;
-	}
-
-	const sort = [...data].sort((a, b) => a.id.localeCompare(b.id));
-	let items = sort.slice(offset, offset + limit);
-	let updatedLinks = UpdateListOfCoursesLinks(items);
-
-	res.status(200).json({
-		total: data.length,
-		limit,
-		offset,
-		items: updatedLinks,
-	});
-});
